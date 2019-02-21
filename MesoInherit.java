d243072ea14428bc8b9d8730dfcee3e65763915e
Lowry West <4lowrywest@gmail.com>
2019-02-21 10:48:58 -0600

    MesoInherit implemented methods from abstract

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,5 +1,17 @@
 //TODO
 public class MesoInherit extends MesoAbstract
 {
+
+	@Override
+	public int[] calAverage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char letterAverage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 }
