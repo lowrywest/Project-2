0317669f65268434149a1e8e05cfcf234b3bfd44
Lowry West <4lowrywest@gmail.com>
2019-02-21 10:56:30 -0600

    Changing input to known output

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args) throws IOException 
 	{
-		String StID = "OKCE";
+		String StID = "NRMN";
 		MesoInherit mesoinherit = new MesoInherit(new MesoStation(StID));
 		
 		System.out.println("Ascii Ceiling is: " + mesoinherit.calAverage()[0]
