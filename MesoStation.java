cd46ea660f053348fb2d208c4e8a136cb193ffc1
Lowry West <4lowrywest@gmail.com>
2019-03-05 12:03:10 -0600

    MesoStation documentation

--- MesoStation.java
+++ MesoStation.java
@@ -1,10 +1,13 @@
 
 public class MesoStation 
 {
+	//Creating the private String variable to use when creating the MesoStation object
 	private String StID;
 	
+	//The constructor for the MesoStation object 
 	public MesoStation(String StID)
 	{
+		//Setting the global variable to the value from the object
 		this.StID = StID;
 	}
 
