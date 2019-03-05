ef0da10b89aac6d10cef6a708d3de8f777c5920d
Lowry West <4lowrywest@gmail.com>
2019-03-05 11:59:55 -0600

    MesoAbstract documentation

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -1,7 +1,8 @@
 public abstract class MesoAbstract 
 {
+	//Creating the calAverage method with the return type of an array of ints
 	public abstract int[] calAverage();
-	
+	//Creating the letterAverage method with the return type of a char
 	public abstract char letterAverage();
 
    //TODO	
